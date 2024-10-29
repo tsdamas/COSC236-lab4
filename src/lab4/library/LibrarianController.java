4e8c7eb910ceae4b6d4cfd406a031a63c1df78ba
Munashe Sydney <munashesydney@github.com>
2024-10-29 15:58:19 -0700

    Fixed variable name error in LibrarianController

--- src/lab4/library/LibrarianController.java
+++ src/lab4/library/LibrarianController.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class LibrarianController {
-	
-	private LIbrary library;
+	private Library library;
 	
 	public LibrarianController(Library library) {
 		this.library = library;
